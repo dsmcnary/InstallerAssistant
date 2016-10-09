eb43d58c531073d81d3d4c990125ae744b2f7ec9
dsmcnary <dsmcnary@northmo.net>
2016-10-09 16:44:21 -0500

    TowerActivity GoogleApiClient Sample Code Implimented and Compiled Sucessfully

--- app/src/main/java/net/northmo/dsmcnary/installerassistant/TowerActivity.java
+++ app/src/main/java/net/northmo/dsmcnary/installerassistant/TowerActivity.java
@@ -1,14 +1,37 @@
 package net.northmo.dsmcnary.installerassistant;
 
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.drive.*;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-
-public class TowerActivity extends AppCompatActivity {
+public class TowerActivity extends FragmentActivity implements OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tower);
+
+        // Create a GoogleApiClient instance
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .build();
+
+        // ...
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // An unresolvable error has occurred and a connection to Google APIs
+        // could not be established. Display an error message, or handle
+        // the failure silently
+
+        // ...
     }
 }
